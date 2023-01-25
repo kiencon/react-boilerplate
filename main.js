613a2f63c4f87da058c328acf58d01de27c72f05
DESKTOP-9L6RD9A <vietson1610@gmail.com>
2023-01-25 13:26:21 +0700

    init

---
+++ main.js
@@ -0,0 +1,5 @@
+const Hello = () => {
+  return <h1>Hello ReactJS!</h1>;
+};
+
+ReactDOM.createRoot(document.getElementById('root')).render(<Hello />);
---
+++ readme.md
@@ -0,0 +1,3 @@
+# branch
+## using CDN
+Initialize React App project by React v18, react-dom, and babel (CDN links)
